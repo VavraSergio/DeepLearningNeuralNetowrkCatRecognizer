949b240323575112961193ee4cb9e4e8822b041b
Sergio Vavra <sergio.vavra@gmail.com>
2024-07-26 23:59:02 +0200

    Updated cost function

--- HelperFunctions.py
+++ HelperFunctions.py
@@ -252,13 +252,6 @@ def L_model_forward(X, parameters):
 def compute_cost(AL, Y):
     """
     Implement the cost function defined by equation (7).
-
-    Arguments:
-    AL -- probability vector corresponding to your label predictions, shape (1, number of examples)
-    Y -- true "label" vector (for example: containing 0 if non-cat, 1 if cat), shape (1, number of examples)
-
-    Returns:
-    cost -- cross-entropy cost
     """
     
     m = Y.shape[1]
